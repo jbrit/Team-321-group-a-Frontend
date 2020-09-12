1cf7a7f1dcd279a38b9934705aba459b67ab72fc
NDIFE <stilesndife@gmail.com>
2020-09-12 01:45:34 +0100

    added sign-in, sign-up and forgot password page with validator

---
+++ src/pages/forgot-password/forgot-password.page.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+import { Field, reduxForm } from "redux-form";
+
+import {
+  Form,
+  Icon,
+  Button,
+  Grid,
+  Segment,
+  Header,
+} from "semantic-ui-react";
+
+import { LabelInputField } from "react-semantic-redux-form";
+
+import { validate } from "../validator";
+
+const ForgotPassword = (props) => {
+  const { handleSubmit, submitting } = props;
+
+  const onSubmit = (props) => {
+    console.log(props);
+  };
+
+  return (
+    <Grid centered columns={2}>
+      <Grid.Column>
+        <Header as="h2" textAlign="center">
+         Forgot Password?
+        </Header>
+        <Segment>
+          <Form size="large" onSubmit={handleSubmit(onSubmit)}>
+            <Field
+              fluid
+              name="email"
+              component={LabelInputField}
+              label={{
+                content: <Icon color="blue" name="mail" size="large" />,
+              }}
+              labelPosition="left"
+              placeholder="Enter Email Here"
+            />
+            <Button color="blue" submitting={submitting} fluid size="large">
+              Recover Password
+            </Button>
+          </Form>
+        </Segment>
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default reduxForm({
+  form: "forgotPasswordForm", // a unique identifier for this form
+  validate,
+})(ForgotPassword);
---
+++ src/pages/validator.js
@@ -0,0 +1,31 @@
+export const validate = values => {
+    const errors = {}
+
+    if(!values.fullname){
+      errors.fullname = 'Required'
+    }else if (Number(values.fullname.length) < 2) {
+      errors.fullname = 'Invalid name'
+    }
+
+    if(!values.phone){
+      errors.phone = 'Required'
+    }else if (!(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]*$/g.test(values.phone))) {
+      errors.phone = 'Invalid phone number'
+    }else if (Number(values.phone.length) <6){
+      errors.phone = 'Invalid phone number'
+    }
+  
+    if (!values.email) {
+      errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address'
+    }
+  
+    if(!values.password){
+      errors.password = 'Required'
+    }else if (Number(values.password.length) < 5) {
+      errors.password = 'Password must not be less than 5'
+    }
+  
+    return errors
+  }
