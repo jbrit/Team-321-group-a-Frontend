acd87de75e131f2e7047652fe927d21a38ddb6c6
NDIFE <stilesndife@gmail.com>
2020-09-12 01:48:35 +0100

    added redux support to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Header from "./components/header/header.component";
+import HomePage from "./pages/homepage/homepage.page";
+import SignIn from "./pages/sign-in/sign-in.page";
+import SignUp from "./pages/sign-up/sign-up.page";
+import forgotPasswordPage from './pages/forgot-password/forgot-password.page';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome</h1>
-      <p>Get connected to a doctor Asap.</p>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/home" component={HomePage} />
+        <Route exact path="/forpass" component={forgotPasswordPage} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/" component={SignIn} />
+      </Switch>
     </div>
   );
 }
